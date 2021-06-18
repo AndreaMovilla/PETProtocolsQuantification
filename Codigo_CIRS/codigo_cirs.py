cb3f01d245a4d60e614336ed7b2c434af1f7d012
Andrea Movilla <>
2021-06-18 20:58:39 +0200

    prueba commit

--- Codigo_CIRS/codigo_cirs.py
+++ Codigo_CIRS/codigo_cirs.py
@@ -12,7 +12,7 @@ from functions import *
 import pandas as pd
 import csv
 
-
+a=10+3
 direct=directories('/Users/andreamovilla/Desktop/TFM_Respiratory_NRRD/D01_CIRS/segmentaciones')
 seg=importnrrd('/Users/andreamovilla/Desktop/TFM_Respiratory_NRRD/D01_CIRS/segmentaciones')
 segref=importnrrd('/Users/andreamovilla/Desktop/TFM_Respiratory_NRRD/D01_CIRS/M04_Static/SEGREFERENCIA2')
